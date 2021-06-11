4c9ce3ce107881475ae4aaad66d21968dc42c7dc
annenotarobot <anne.marx@unibw.de>
2021-06-11 11:02:44 +0200

    color and global theme defined

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatDialogModule } from '@angular/material/dialog';
 
 import { AppComponent } from './app.component';
+import { PopupComponent } from './utils/popup/popup.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PopupComponent
   ],
   imports: [
-    BrowserModule, HttpClientModule
+    BrowserModule, HttpClientModule,
+    MatDialogModule, MatInputModule,
+    MatButtonModule, MatCardModule, MatFormFieldModule, BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
