b4d4b4ffccefe9325da53ab6ccd2f9701488c143
AngelskijeGlazki <54185047+AngelskijeGlazki@users.noreply.github.com>
2021-06-14 20:34:05 +0200

    organisations new (with underorganisations etc)

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'inforadiator-theses'`, () => {
+  it(`should have as title 'abschlussarbeiten'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('inforadiator-theses');
+    expect(app.title).toEqual('abschlussarbeiten');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('inforadiator-theses app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('abschlussarbeiten app is running!');
   });
 });
--- src/app/communitymashup/model/organisation.model.ts
+++ src/app/communitymashup/model/organisation.model.ts
@@ -6,19 +6,46 @@ export class Organisation extends InformationObject {
 
   // no additional attributes
   // additional references
-  // leader:Person, parentOrganisation:Organisation, participants:Person, organizations:Organisation
+  // parentOrganisation:Organisation, members:Person, organizations:Organisation
+  parentOrganisation: string = null;
+  organisations: string[] = [];
+  members: string[] = [];
 
   constructor(item, public service: CommunityMashupService) {
     super(item, service);
+    // reference parentOrganisation
+    this.parentOrganisation = item['parentOrganisation'];
+    // reference organisations
+    var tmps = item['organisations'];
+    if(tmps) {
+      var tmpsArr = tmps.split(" ");
+      tmpsArr.forEach(id => this.organisations.push(id));
+    }
+    // reference members
+    tmps = item['members'];
+    if(tmps) {
+      var tmpsArr = tmps.split(" ");
+      tmpsArr.forEach(id => this.members.push(id));
+    }
   }
 
-  getLeader(): Person {
-    // TBD
+  getParentOrganisation(): Organisation {
+    if (this.parentOrganisation) {
+      return this.service.getItemById(this.parentOrganisation);
+    }
     return null;
   }
 
-  // TBD: getParentOrganisation
-  // TBD: getParticipants
-  // TBD: getOrganisations
+  getChildOrganisations(): Organisation[] {
+    var result: Organisation[] = [];
+    this.organisations.forEach(id => result.push(this.service.getItemById(id)));
+    return result;
+  }
+
+  getMembers(): Person[] {
+    var result: Person[] = [];
+    this.members.forEach(id => result.push(this.service.getItemById(id)));
+    return result;
+  }
 
 }
